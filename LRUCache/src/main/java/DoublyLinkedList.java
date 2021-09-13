e292cc0346a088884b740540f64eeebf99aab11c
tarasska <taras.skazhenik@yandex.ru>
2021-09-13 21:04:07 +0300

    feat: added doubly linked list

---
+++ LRUCache/src/main/java/DoublyLinkedList.java
@@ -0,0 +1,5 @@
+public interface DoublyLinkedList<T> {
+    void addFirst(T value);
+    T removeLast();
+    boolean remove(T value);
+}
---
+++ LRUCache/src/main/java/SequentialList.java
@@ -0,0 +1,74 @@
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SequentialList<T> implements DoublyLinkedList<T> {
+    private static class Node<E> {
+        private final E value;
+
+        private Node<E> prev;
+        private Node<E> next;
+
+        Node() {
+            this.value = null;
+            this.next = this;
+            this.prev = this;
+        }
+
+        Node(E value, Node<E> prev, Node<E> next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private int size = 0;
+    private final Node<T> head = new Node<>();
+
+    @Override
+    public void addFirst(T value) {
+        Node<T> newNode = new Node<>(value, head.prev, head);
+
+        assert head.prev != null && head.prev.next != null
+                : "There are no null pointers in the cyclic list";
+        head.prev.next = newNode;
+        head.prev = newNode;
+
+        size++;
+    }
+
+    @Override
+    public T removeLast() {
+        if (size == 0) {
+            throw new NoSuchElementException("List is empty");
+        }
+        Node<T> last = head.prev;
+
+        assert last.next != null && last.next.prev != null
+            : "There are no null pointers in the cyclic list";
+        last.next.prev = head;
+        head.prev = last.next;
+
+        size--;
+        return last.value;
+    }
+
+    @Override
+    public boolean remove(T value) {
+        Node<T> curNode = head.next;
+
+        while (curNode != head) {
+            assert curNode != null : "There are no null pointers in the cyclic list";
+            if (Objects.equals(value, curNode.value)) {
+                assert curNode.prev != null && curNode.prev.next != null
+                    : "There are no null pointers in the cyclic list";
+                curNode.prev.next = curNode.next;
+                curNode.next.prev = curNode.prev;
+
+                size--;
+                return true;
+            }
+            curNode = curNode.next;
+        }
+        return false;
+    }
+}
