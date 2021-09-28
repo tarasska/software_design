4caf71c57874e98fab65983ecaa481f2a072610e
tarasska <taras.skazhenik@yandex.ru>
2021-09-28 23:54:11 +0300

    test: updated test

--- refactoring/src/test/java/ru/akirakozov/sd/refactoring/servlet/AddProductServletTest.java
+++ refactoring/src/test/java/ru/akirakozov/sd/refactoring/servlet/AddProductServletTest.java
@@ -5,7 +5,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import java.io.IOException;
+import java.io.StringWriter;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 
@@ -13,12 +15,13 @@ public class AddProductServletTest extends BaseServletRealDBTest {
 
     @Test
     public void doGetNoExcept() throws IOException {
-        super.doGetNoExceptDefaultResponse(() -> {
+        super.doGetNoExceptDefaultResponse((StringWriter writer) -> {
             try {
                 Mockito.when(request.getParameter("name")).thenReturn("iphone6");
                 Mockito.when(request.getParameter("price")).thenReturn("30000");
 
                 new AddProductServlet().doGet(request, response);
+                assertTrue(writer.toString().contains("OK"));
             } catch (IOException e) {
                 fail("Unexpected exception", e);
             }
