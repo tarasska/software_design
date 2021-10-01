e0a045cefd063e0646e84a5fa797977821406ee3
tarasska <skazhenik.taras@niuitmo.ru>
2021-10-01 14:23:01 +0300

    feat: added dao example

---
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/dao/ProductDao.java
@@ -0,0 +1,24 @@
+package ru.akirakozov.sd.refactoring.dao;
+
+import ru.akirakozov.sd.refactoring.database.DBManager;
+import ru.akirakozov.sd.refactoring.entities.Product;
+
+import java.sql.SQLException;
+
+public class ProductDao {
+    private final DBManager dbManager;
+
+    public ProductDao(DBManager dbManager) {
+        this.dbManager = dbManager;
+    }
+
+    public void addProduct(Product product) throws SQLException {
+        dbManager.executeQuery(statement -> {
+            String sql = "INSERT INTO PRODUCT " +
+                    "(NAME, PRICE) VALUES (\"" + product.getName() + "\"," + product.getPrice() + ")";
+            statement.executeUpdate(sql);
+            statement.close();
+            return null;
+        });
+    }
+}
