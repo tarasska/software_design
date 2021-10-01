48c9f0d7c9d26f95e2eace8e77c9aa2f2fbfb53c
tarasska <skazhenik.taras@niuitmo.ru>
2021-10-01 14:22:42 +0300

    feat: added query execution to db manager

--- refactoring/src/main/java/ru/akirakozov/sd/refactoring/database/DBManager.java
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/database/DBManager.java
@@ -2,7 +2,19 @@ package ru.akirakozov.sd.refactoring.database;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public interface DBManager {
+    @FunctionalInterface
+    interface CheckedFun<T, R> {
+        R apply(T t) throws SQLException;
+    }
+
     Connection getConnection() throws SQLException;
+
+    default <R> R executeQuery(CheckedFun<Statement, R> query) throws SQLException {
+        try (Connection c = getConnection()) {
+            return query.apply(c.createStatement());
+        }
+    }
 }
--- refactoring/src/main/java/ru/akirakozov/sd/refactoring/database/DBManagerImpl.java
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/database/DBManagerImpl.java
@@ -3,6 +3,7 @@ package ru.akirakozov.sd.refactoring.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBManagerImpl implements DBManager {
     private final String connectionUrl;
