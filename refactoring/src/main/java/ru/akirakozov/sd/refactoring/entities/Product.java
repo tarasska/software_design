9e963c8daa8da941df0cb0cf36e67bbdfae01d89
tarasska <taras.skazhenik@yandex.ru>
2021-10-04 20:48:50 +0300

    feat: implement equals for Product

--- refactoring/src/main/java/ru/akirakozov/sd/refactoring/entities/Product.java
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/entities/Product.java
@@ -1,10 +1,13 @@
 package ru.akirakozov.sd.refactoring.entities;
 
+import java.util.Objects;
+
 public class Product {
     private final String name;
     private final long price;
 
     public Product(String name, long price) {
+        Objects.requireNonNull(name);
         this.name = name;
         this.price = price;
     }
@@ -15,5 +18,14 @@ public class Product {
 
     public long getPrice() {
         return price;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Product) {
+            Product other = (Product) obj;
+            return name.equals(other.name) && price == other.price;
+        }
+        return false;
     }
 }
