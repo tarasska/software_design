e792f3ca43f8b9c242b3db4dff62676c52c60d49
tarasska <taras.skazhenik@yandex.ru>
2021-10-04 11:28:58 +0300

    feat: added methods for html building

--- refactoring/src/main/java/ru/akirakozov/sd/refactoring/dao/ProductDao.java
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/dao/ProductDao.java
@@ -14,10 +14,36 @@ public class ProductDao {
         this.dbManager = dbManager;
     }
 
+    private void buildDefaultHtml(PrintWriter writer, DBManager.CheckedConsumer<PrintWriter> bodyBuilder) throws SQLException {
+        writer.println("<html><body>");
+        bodyBuilder.accept(writer);
+        writer.println("</body></html>");
+    }
+
+    private void buildFilteredProductHtml(PrintWriter writer, ResultSet rs, String header) throws SQLException {
+        buildDefaultHtml(writer, w -> {
+            w.println(header);
+            while (rs.next()) {
+                String name = rs.getString("name");
+                int price = rs.getInt("price");
+                w.println(name + "\t" + price + "</br>");
+            }
+        });
+    }
+
+    private void buildInfoHtml(PrintWriter writer, ResultSet rs, String header) throws SQLException {
+        buildDefaultHtml(writer, w -> {
+            w.println(header);
+            if (rs.next()) {
+                writer.println(rs.getInt(1));
+            }
+        });
+    }
+
     public void addProduct(Product product) throws SQLException {
         dbManager.executeQuery(statement -> {
             String sql = "INSERT INTO PRODUCT " +
-                    "(NAME, PRICE) VALUES (\"" + product.getName() + "\"," + product.getPrice() + ")";
+                "(NAME, PRICE) VALUES (\"" + product.getName() + "\"," + product.getPrice() + ")";
             statement.executeUpdate(sql);
         });
     }
@@ -28,8 +54,8 @@ public class ProductDao {
                 writer.println("<html><body>");
 
                 while (rs.next()) {
-                    String  name = rs.getString("name");
-                    int price  = rs.getInt("price");
+                    String name = rs.getString("name");
+                    int price = rs.getInt("price");
                     writer.println(name + "\t" + price + "</br>");
                 }
                 writer.println("</body></html>");
