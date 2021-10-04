55114ad4863dcdd5220f1ad75beea199691db0d7
tarasska <taras.skazhenik@yandex.ru>
2021-10-04 15:20:05 +0300

    refactor: moved db related methods to base test class

---
+++ refactoring/src/test/java/ru/akirakozov/sd/refactoring/RealDBTest.java
@@ -0,0 +1,43 @@
+package ru.akirakozov.sd.refactoring;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class RealDBTest {
+
+    private static String TEST_DB_URL = "jdbc:sqlite:src/test/resources/test.db";
+
+    private static void executeSql(String query) throws SQLException {
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:src/test/resources/test.db")) {
+            Statement stmt = c.createStatement();
+
+            stmt.executeUpdate(query);
+            stmt.close();
+        }
+    }
+    @BeforeAll
+    public static void setup() throws SQLException {
+        executeSql(
+            "CREATE TABLE IF NOT EXISTS PRODUCT" +
+            "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+            " NAME           TEXT    NOT NULL, " +
+            " PRICE          INT     NOT NULL)"
+        );
+    }
+
+    @AfterAll
+    public static void destroy() throws SQLException {
+        executeSql("DROP TABLE IF EXISTS PRODUCT");
+    }
+
+    @BeforeEach
+    public void clearDB() throws SQLException {
+        executeSql("DELETE FROM PRODUCT");
+    }
+}
--- refactoring/src/test/java/ru/akirakozov/sd/refactoring/servlet/BaseServletRealDBTest.java
+++ refactoring/src/test/java/ru/akirakozov/sd/refactoring/servlet/BaseServletRealDBTest.java
@@ -6,69 +6,29 @@ import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import ru.akirakozov.sd.refactoring.RealDBTest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.function.Consumer;
 
 
-public class BaseServletRealDBTest {
+public class BaseServletRealDBTest extends RealDBTest {
     @Mock
     protected HttpServletRequest request;
 
     @Mock
     protected HttpServletResponse response;
 
-    @BeforeAll
-    public static void setup() throws SQLException {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:src/test/resources/test.db")) {
-            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
-                "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
-                " NAME           TEXT    NOT NULL, " +
-                " PRICE          INT     NOT NULL)";
-            Statement stmt = c.createStatement();
-
-            stmt.executeUpdate(sql);
-            stmt.close();
-        }
-    }
-
-    @AfterAll
-    public static void destroy() throws SQLException {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:src/test/resources/test.db")) {
-            String sql = "DROP TABLE IF EXISTS PRODUCT";
-            Statement stmt = c.createStatement();
-
-            stmt.executeUpdate(sql);
-            stmt.close();
-        }
-    }
-
     @BeforeEach
     public void initMock() {
         MockitoAnnotations.openMocks(this);
     }
 
-    @BeforeEach
-    public void clearDB() throws SQLException {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:src/test/resources/test.db")) {
-            String sql = "DELETE FROM PRODUCT";
-            Statement stmt = c.createStatement();
-
-            stmt.executeUpdate(sql);
-            stmt.close();
-        }
-    }
-
-    public void doGetNoExceptDefaultResponse(Consumer<StringWriter> test)
-        throws IOException {
+    public void doGetNoExceptDefaultResponse(Consumer<StringWriter> test) throws IOException {
         StringWriter stringWriter = new StringWriter();
         PrintWriter writer = new PrintWriter(stringWriter);
         Mockito.when(response.getWriter()).thenReturn(writer);
