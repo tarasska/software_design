effaf85c13b03452fb1d634b3a4d026ab47da4f0
tarasska <taras.skazhenik@yandex.ru>
2021-10-08 19:58:05 +0300

    build: added lib for html templates

--- todo-list/build.gradle.kts
+++ todo-list/build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
     mavenCentral()
 }
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-mustache")
     implementation("org.springframework.boot:spring-boot-starter-web")
