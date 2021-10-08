5b77eb1041f4954835759757f7ffbf9dccd64524
tarasska <taras.skazhenik@yandex.ru>
2021-10-08 22:38:37 +0300

    feat: added list creation

--- todo-list/src/main/kotlin/mvc_application/controller/TodoListController.kt
+++ todo-list/src/main/kotlin/mvc_application/controller/TodoListController.kt
@@ -2,6 +2,8 @@ package mvc_application.controller
 
 import mvc_application.dao.Storage
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 
@@ -10,7 +12,14 @@ class TodoListController(
     private val taskListStorage: Storage
 ) {
     @PostMapping("/add-list")
-    fun addList(@RequestParam name: String) {
+    fun addList(@RequestParam name: String): String {
         taskListStorage.createTaskList(name)
+        return "redirect:/get-lists"
+    }
+
+    @GetMapping("/get-lists")
+    fun getLists(map: Model): String {
+        map.addAttribute("taskLists", taskListStorage.getAllLists())
+        return "index"
     }
 }
