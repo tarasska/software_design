a5e702eefd3568403c63c6d69d63d3d07c4a9912
tarasska <taras.skazhenik@yandex.ru>
2021-10-08 22:38:12 +0300

    feat: implemented storage base methods

--- todo-list/src/main/kotlin/mvc_application/dao/InMemoryStorage.kt
+++ todo-list/src/main/kotlin/mvc_application/dao/InMemoryStorage.kt
@@ -3,23 +3,39 @@ package mvc_application.dao
 import mvc_application.model.Task
 import mvc_application.model.TaskList
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
 
 class InMemoryStorage : Storage {
     private val nameToLists: MutableMap<String, TaskList> = ConcurrentHashMap()
-    private val idToListNames: MutableMap<Int, String> = ConcurrentHashMap()
+    private val nameToTaskIdGen: MutableMap<String, AtomicInteger> = ConcurrentHashMap()
+
     override fun createTaskList(name: String) {
         nameToLists.putIfAbsent(name, TaskList(name))
     }
 
     override fun deleteTaskList(name: String) {
-        TODO("Not yet implemented")
+        nameToLists.remove(name)
     }
 
-    override fun addTask(listName: String, task: Task): Int {
-        TODO("Not yet implemented")
+    override fun getAllLists(): List<TaskList> {
+        return nameToLists.values.toList()
+    }
+
+    override fun findListByName(name: String): TaskList? {
+        return nameToLists[name]
+    }
+
+    override fun addTask(listName: String, header: String, content: String): Int {
+        val list = nameToLists[listName]
+        if (list === null) {
+            throw NoSuchElementException("List with provided name=$listName not found.")
+        }
+        val id = nameToTaskIdGen[listName]!!.getAndIncrement()
+        list.addTask(Task(id, header, content))
+        return id
     }
 
     override fun removeTask(listName: String, taskId: Int) {
-        TODO("Not yet implemented")
+        nameToLists[listName]?.removeTask(taskId)
     }
 }
--- todo-list/src/main/kotlin/mvc_application/dao/Storage.kt
+++ todo-list/src/main/kotlin/mvc_application/dao/Storage.kt
@@ -1,11 +1,14 @@
 package mvc_application.dao
 
-import mvc_application.model.Task
+import mvc_application.model.TaskList
 
 interface Storage {
     fun createTaskList(name: String)
     fun deleteTaskList(name: String)
 
-    fun addTask(listName: String, task: Task): Int
+    fun getAllLists(): List<TaskList>
+    fun findListByName(name: String): TaskList?
+
+    fun addTask(listName: String, header: String, content: String): Int
     fun removeTask(listName: String, taskId: Int)
 }
