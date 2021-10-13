b4ca0ff7abec70b8ba334717d8bc0d6ee50ff0b3
tarasska <taras.skazhenik@yandex.ru>
2021-10-13 22:09:12 +0300

    feat: added simple impl for renderers

--- bridge-graph/src/main/kotlin/rendering/impl/AwtRenderer.kt
+++ bridge-graph/src/main/kotlin/rendering/impl/AwtRenderer.kt
@@ -3,23 +3,30 @@ package rendering.impl
 import rendering.RenderingApi
 import rendering.model.Point
 import java.awt.Graphics2D
+import java.awt.geom.Ellipse2D
+import java.awt.geom.Line2D
 
-class AwtRenderer<T>(
-    private val graphics2d: Graphics2D
-) : RenderingApi<T> {
-    override fun getDrawingAreaWidth(): Long {
-        TODO("Not yet implemented")
+class AwtRenderer(
+    private val graphics2d: Graphics2D,
+    private val height: Long,
+    private val width: Long
+) : RenderingApi<Double> {
+    override fun getDrawingAreaWidth(): Long = width
+
+    override fun getDrawingAreaHeight(): Long = height
+
+    override fun drawCircle(point: Point<Double>, radius: Double) {
+        graphics2d.fill(
+            Ellipse2D.Double(
+                point.getX() - radius, point.getY() - radius,
+                2 * radius, 2 * radius
+            )
+        )
     }
 
-    override fun getDrawingAreaHeight(): Long {
-        TODO("Not yet implemented")
-    }
-
-    override fun drawCircle(point: Point<T>, radius: T) {
-        TODO("Not yet implemented")
-    }
-
-    override fun drawLine(from: Point<T>, to: Point<T>) {
-        TODO("Not yet implemented")
+    override fun drawLine(from: Point<Double>, to: Point<Double>) {
+        graphics2d.draw(
+            Line2D.Double(from.getX(), from.getY(), to.getX(), to.getY())
+        )
     }
 }
--- bridge-graph/src/main/kotlin/rendering/impl/JavaFXRenderer.kt
+++ bridge-graph/src/main/kotlin/rendering/impl/JavaFXRenderer.kt
@@ -1,4 +1,26 @@
 package rendering.impl
 
-class JavaFXRenderer {
+import javafx.scene.canvas.Canvas
+import rendering.RenderingApi
+import rendering.model.Point
+
+class JavaFXRenderer(
+    private val canvas: Canvas,
+    private val height: Long,
+    private val width: Long
+) : RenderingApi<Double> {
+    override fun getDrawingAreaWidth(): Long = height
+
+    override fun getDrawingAreaHeight(): Long = width
+
+    override fun drawCircle(point: Point<Double>, radius: Double) {
+        canvas.graphicsContext2D.fillOval(
+            point.getX() - radius, point.getY() - radius,
+            2 * radius, 2 * radius
+        )
+    }
+
+    override fun drawLine(from: Point<Double>, to: Point<Double>) {
+        canvas.graphicsContext2D.strokeLine(from.getX(), from.getY(), to.getX(), to.getY())
+    }
 }
