248d9e5ef06d5152358866a63c231900add034fc
tarasska <taras.skazhenik@yandex.ru>
2021-10-15 23:28:38 +0300

    feat: added javafx application

---
+++ bridge-graph/src/main/kotlin/rendering/impl/app/JavaFXApplicationRunner.kt
@@ -0,0 +1,49 @@
+package rendering.impl.app
+
+import graph.Graph
+import javafx.application.Application
+import javafx.application.Application.launch
+import javafx.scene.Group
+import javafx.scene.Scene
+import javafx.scene.canvas.Canvas
+import javafx.scene.paint.Color
+import javafx.stage.Stage
+import rendering.RenderingApi
+import rendering.impl.api.JavaFXRenderer
+
+class JavaFXApplicationRunner(
+    canvasHeight: Int,
+    canvasWidth: Int,
+    constructGraph: (RenderingApi<Double>) -> Graph
+): Runnable {
+    class JavaFXApplication : Application() {
+        override fun start(primaryStage: Stage) {
+            val canvas = Canvas(staticHeight.toDouble(), staticWidth.toDouble())
+            primaryStage.scene = Scene(Group(canvas), Color.CORAL)
+            staticGraphConstructor(
+                JavaFXRenderer(
+                    canvas,
+                    staticHeight.toLong(),
+                    staticWidth.toLong()
+                )
+            ).renderGraph()
+            primaryStage.show()
+        }
+    }
+
+    init {
+        staticHeight = canvasHeight
+        staticWidth = canvasWidth
+        staticGraphConstructor = constructGraph
+    }
+
+    companion object {
+        private var staticHeight: Int = 400
+        private var staticWidth: Int = 600
+        private lateinit var staticGraphConstructor: (RenderingApi<Double>) -> Graph
+    }
+
+    override fun run() {
+        launch(JavaFXApplication::class.java)
+    }
+}
