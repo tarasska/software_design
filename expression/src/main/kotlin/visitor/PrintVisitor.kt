d2ea6c719039118a1b15c05b717042b8b78fb0c2
tarasska <taras.skazhenik@yandex.ru>
2021-10-18 21:16:08 +0300

    fix: fixed incorrect printing

--- expression/src/main/kotlin/visitor/PrintVisitor.kt
+++ expression/src/main/kotlin/visitor/PrintVisitor.kt
@@ -9,8 +9,10 @@ class PrintVisitor(private val output: Writer) : TokenVisitor<Unit> {
 
     override fun visit(token: OperationToken) = output.write(token.show())
 
-    override fun visitAll(tokens: List<Token>) = tokens.forEach { t ->
-        t.accept(this)
-        output.write(" ")
+    override fun visitAll(tokens: List<Token>) {
+        ParserVisitor().visitAll(tokens).forEach { t ->
+            t.accept(this)
+            output.write(" ")
+        }
     }
 }
