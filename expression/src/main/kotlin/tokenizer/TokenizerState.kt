7dbe956e3b8df38a0e43f0fab430128922881244
tarasska <skazhenik.taras@niuitmo.ru>
2021-10-18 12:17:26 +0300

    feat: added tokenizer states

---
+++ expression/src/main/kotlin/tokenizer/TokenizerState.kt
@@ -0,0 +1,49 @@
+package tokenizer
+
+sealed class TokenizerState {
+    protected val tokens = mutableListOf<Token>()
+
+    fun getActualTokens(): List<Token> = tokens
+
+    abstract fun nextChar(ch: Char): TokenizerState
+
+}
+
+class State : TokenizerState() {
+
+    private fun selfState(token: Token): TokenizerState {
+        tokens.add(token)
+        return this
+    }
+
+    override fun nextChar(ch: Char): TokenizerState = when (ch) {
+        '+' -> selfState(ADD)
+        '-' -> selfState(SUB)
+        '*' -> selfState(MUL)
+        '/' -> selfState(DIV)
+        '(' -> selfState(LEFT)
+        ')' -> selfState(RIGHT)
+        in '0'..'9' -> NumberState(ch.digitToInt())
+        else -> FailedState(ch)
+    }
+}
+
+class NumberState(
+    private val number: Int
+) : TokenizerState() {
+
+    override fun nextChar(ch: Char): TokenizerState = when (ch) {
+        in '0'..'9' -> NumberState(number * 10 + ch.digitToInt())
+        else -> {
+            tokens.add(NumberToken(number))
+            State().nextChar(ch)
+        }
+    }
+}
+
+class FailedState(ch: Char): TokenizerState() {
+
+    val message = "Unable to tokenize char: $ch"
+
+    override fun nextChar(ch: Char): TokenizerState = this
+}
