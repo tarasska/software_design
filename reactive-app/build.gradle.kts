2114a97b3024fc31a5c7d2afeefe40eea8527a46
tarasska <taras.skazhenik@yandex.ru>
2022-03-06 15:10:12 +0300

    build: added rx deps

--- reactive-app/build.gradle.kts
+++ reactive-app/build.gradle.kts
@@ -12,6 +12,17 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
+
+
+    implementation("org.mongodb", "mongodb-driver-rx", "1.5.0")
+    implementation("io.reactivex:rxnetty-common:0.5.3")
+    implementation("io.reactivex:rxnetty-http:0.5.3")
+    implementation("io.netty:netty-all:4.0.48.Final")
+
+    implementation("io.reactivex.rxjava3:rxjava:3.0.1")
+    implementation("io.reactivex.rxjava3:rxkotlin:3.0.1")
+
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
