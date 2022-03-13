d13b2e400aa2cf171e7e7e6e6f847dea5d241a73
tarasska <taras.skazhenik@yandex.ru>
2022-03-13 16:02:02 +0300

    feat: implement StockExchangeDao

--- exchange-container/stock-exchange/src/main/java/db/StockExchangeDao.java
+++ exchange-container/stock-exchange/src/main/java/db/StockExchangeDao.java
@@ -1,42 +1,93 @@
 package db;
 
+import com.mongodb.client.model.Filters;
+import com.mongodb.rx.client.FindObservable;
 import com.mongodb.rx.client.MongoCollection;
 import com.mongodb.rx.client.Success;
 import model.Company;
 import org.bson.Document;
 import rx.Observable;
 
+import java.util.function.Function;
+
 public class StockExchangeDao implements StockExchange {
 
     private final MongoCollection<Document> companies = MongoDB.getCompanies();
 
+    private <T extends Document> Observable<Company> mapCompany(FindObservable<T> found) {
+        return found.toObservable().map(Company::fromDocument);
+    }
+
+    private Observable<Company> findCompany(String company) {
+        return mapCompany(companies.find(Filters.eq(Company.COMPANY_KEY, company))).defaultIfEmpty(null);
+    }
+
+    private Observable<Success> replaceIfNonNull(
+        String companyName,
+        Function<Company, Company> mapper
+    ) {
+        return findCompany(companyName).flatMap(company -> {
+            if (company == null) {
+                return Observable.error(new IllegalArgumentException(String.format(
+                    "Provided company %s not exists.", companyName
+                )));
+            }
+            Company updatedCompany = mapper.apply(company);
+
+            return companies
+                .replaceOne(
+                    Filters.eq(Company.COMPANY_KEY, companyName),
+                    updatedCompany.toDocument()
+                )
+                .map(document -> Success.SUCCESS);
+        });
+    }
+
     @Override
     public Observable<Success> addCompany(String name, int stocksCount, int stocksPrice) {
-        return null;
+        return findCompany(name).flatMap(company -> {
+           if (company != null) {
+               return Observable.error(new IllegalArgumentException(String.format(
+                   "Unable to add company %s, because it already exist.",
+                   name
+               )));
+           } else {
+               return companies.insertOne(new Company(name, stocksCount, stocksPrice).toDocument());
+           }
+        });
     }
 
     @Override
     public Observable<Success> addStocks(String company, int stocksCount) {
-        return null;
+        return replaceIfNonNull(company, (companyObj) -> {
+            companyObj.addStocks(stocksCount);
+            return companyObj;
+        });
     }
 
     @Override
     public Observable<Company> getCompanies() {
-        return null;
+        return mapCompany(companies.find());
     }
 
     @Override
     public Observable<Company> getCompany(String company) {
-        return null;
+        return findCompany(company);
     }
 
     @Override
     public Observable<Success> updateStockPrices(String company, int newPrice) {
-        return null;
+        return replaceIfNonNull(company, (companyObj) -> {
+            companyObj.setStockPrice(newPrice);
+            return companyObj;
+        });
     }
 
     @Override
     public Observable<Success> buyStocks(String company, int stocksCount) {
-        return null;
+        return replaceIfNonNull(company, (companyObj) -> {
+            companyObj.addStocks(-stocksCount);
+            return companyObj;
+        });
     }
 }
--- exchange-container/stock-exchange/src/main/java/model/Company.java
+++ exchange-container/stock-exchange/src/main/java/model/Company.java
@@ -5,6 +5,10 @@ import org.bson.Document;
 import java.util.Map;
 
 public class Company implements DBEntity {
+    public static final String COMPANY_KEY = "company";
+    public static final String STOCK_CNT_KEY = "stockCount";
+    public static final String STOCK_PRICE_KEY = "stockPrice";
+
     private final String name;
     private int stockCount;
     private int stockPrice;
@@ -17,18 +21,26 @@ public class Company implements DBEntity {
 
     public static Company fromDocument(Document document) {
         return new Company(
-            document.getString("company"),
-            document.getInteger("stockCount"),
-            document.getInteger("stockPrice")
+            document.getString(COMPANY_KEY),
+            document.getInteger(STOCK_CNT_KEY),
+            document.getInteger(STOCK_PRICE_KEY)
         );
     }
 
     @Override
     public Document toDocument() {
         return new Document(Map.of(
-           "company", name,
-           "stockCount", stockCount,
-           "stockPrice", stockPrice
+            COMPANY_KEY, name,
+            STOCK_CNT_KEY, stockCount,
+            STOCK_PRICE_KEY, stockPrice
         ));
+    }
+
+    public void addStocks(int delta) {
+        this.stockCount += delta;
+    }
+
+    public void setStockPrice(int newPrice) {
+        this.stockPrice = newPrice;
     }
 }
