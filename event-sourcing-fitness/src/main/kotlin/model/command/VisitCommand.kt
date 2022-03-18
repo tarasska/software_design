a68e27d96bc8e90ae1fcb0cfe97d719b284c9851
tarasska <taras.skazhenik@yandex.ru>
2022-03-18 21:31:07 +0300

    feat: added visit commands

---
+++ event-sourcing-fitness/src/main/kotlin/model/command/VisitCommand.kt
@@ -0,0 +1,56 @@
+package model.command
+
+import com.mongodb.rx.client.Success
+import db.FitnessCenterDao
+import model.event.VisitEvent
+import rx.Observable
+import java.time.LocalDateTime
+import kotlin.IllegalStateException
+
+class VisitCommand(private val fitnessCenterDao: FitnessCenterDao) {
+
+    private fun checkDoubleEnter(userId: Long, now: LocalDateTime): Observable<Success> {
+        return fitnessCenterDao.getLastVisitEvent(userId)
+            .flatMap { visit ->
+                if (visit.type == VisitEvent.VisitType.EXIT) {
+                    fitnessCenterDao.registerVisit(userId, now, VisitEvent.VisitType.ENTER)
+                    Observable.just(Success.SUCCESS)
+                } else {
+                    Observable.error(IllegalStateException(
+                        "You have already used your card to enter."
+                    ))
+                }
+            }
+    }
+
+    private fun checkSubscription(userId: Long, now: LocalDateTime): Observable<Success> {
+        return fitnessCenterDao.getActualSubscriptionByUserId(userId)
+            .flatMap {
+                if (it.endTime.isBefore(now)) {
+                    checkDoubleEnter(userId, now)
+                } else {
+                    Observable.error(IllegalStateException(
+                        "You don't have a active subscription."
+                    ))
+                }
+            }
+    }
+
+    fun doEnter(userId: Long) : Observable<Success> {
+        val now = LocalDateTime.now()
+        return checkSubscription(userId, now)
+    }
+
+    fun doExit(userId: Long): Observable<Success> {
+        return fitnessCenterDao.getLastVisitEvent(userId)
+            .flatMap {
+                if (it.type == VisitEvent.VisitType.ENTER) {
+                    Observable.just(Success.SUCCESS)
+                } else {
+                    Observable.error(IllegalStateException(
+                        "You should enter before exit."
+                    ))
+                }
+            }
+    }
+}
